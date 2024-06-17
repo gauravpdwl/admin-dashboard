fec250bddb8f7777194a3426ce53e2133f4c2944
gauravpdwl <gauravpadwal45@gmail.com>
2024-06-17 15:29:56 +0530

    Added setup for refreshing the tokens using refresh token

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api=axios.create({
     baseURL:"http://localhost:8000/",
@@ -11,3 +12,33 @@ export const api=axios.create({
     }
 })
 
+// const refreshToken=()=>api.get('auth/refresh');
+
+const refreshToken=async ()=>{
+    await axios.post(`http://localhost:8000/auth/refresh`, 
+        // body
+        {},
+        {
+        withCredentials:true
+    })
+}
+
+api.interceptors.response.use((response)=>response, async (error)=>{
+
+    const originalrequest=error.config;
+
+    if(error.response.status === 401 && !originalrequest._isRetry){
+        try{
+            originalrequest._isRetry=true;
+            const headers={...originalrequest.headers};
+            await refreshToken();
+            return api.request({...originalrequest, headers})
+        }catch(err){
+            console.error("Token refresh error, ",err);
+            useAuthStore.getState().logout();
+            return Promise.reject(err);
+        }
+    }
+
+    return Promise.reject(error);
+});
