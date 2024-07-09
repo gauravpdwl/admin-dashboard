d772dbdf162082733b3e97c0513805a1e7e901e0
gauravpdwl <gauravpadwal45@gmail.com>
2024-07-09 10:50:15 +0530

    added spinner and error for products page

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,5 +1,5 @@
-import { Breadcrumb, Button, Flex, Form, Image, Space, Table, Tag, Typography } from 'antd';
-import { RightOutlined, PlusOutlined } from '@ant-design/icons';
+import { Breadcrumb, Button, Flex, Form, Image, Space, Spin, Table, Tag, Typography } from 'antd';
+import { RightOutlined, PlusOutlined, LoadingOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import ProductsFilter from './productsFilter';
 import { FieldData, Product } from '../../types';
@@ -66,7 +66,10 @@ const Products = () => {
     });
 
     const {
-        data: products
+        data: products,
+        isFetching,
+        isError,
+        error
     } = useQuery({
         queryKey: ['products', queryParams],
         queryFn: () => {
@@ -119,6 +122,13 @@ const Products = () => {
                         separator={<RightOutlined />}
                         items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Products' }]}
                     />
+
+                    {isFetching && (
+                        <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+                    )}
+                    
+                    {isError && <Typography.Text type="danger">{error.message}</Typography.Text>}
+
                 </Flex>
                 {/* onFieldsChange={() => {onFilterChange}} */}
                 <Form form={filterForm} onFieldsChange={onFilterChange}>
